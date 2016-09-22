46703ebaa86177ae9a521929cbdac2ca1fc3c5e1
Paul Walsh <paul.walsh@first-utility.com>
2016-09-22 13:51:55 +0100

    re-coded

---
+++ collect_info.pl
@@ -0,0 +1,58 @@
+
+#====================================================================
+# collect_info	 - Collect useful information
+#====================================================================
+sub collect_info {
+
+   $hostname=`hostname`;
+         $memswap= `free -m`;
+         $hostid=`hostname`;
+         $kernel=`uname -r`;
+         $model= "echo \"N/A\"";
+         $type= "echo \"N/A\"";
+         $blkinfo=`lsblk`;
+         $filesystems= `df -hP`;
+
+   if (-e "/etc/redhat-release") {
+      $OS = `cat /etc/redhat-release`;
+   } else {
+      $OS = `cat /etc/issue`;
+   }
+   chop($hostname);
+   $resolver = new Net::DNS::Resolver();
+   $reply = $resolver->query($hostname,'A');
+   if ($reply) {
+     foreach my $rr ($reply->answer) {
+       next unless $rr->type eq "A";
+      $ipaddr=$rr->address;
+     }
+   }
+}
+
+#====================================================================
+# write_info	 - write info to file
+#====================================================================
+#
+sub write_info {
+print"#############################\n";
+print"######## SYSTEM INFO ########\n";
+print"#############################\n\n";
+printf("Hostname:     %s",$hostname);
+printf("IP Address:   %s",$ipaddr);
+printf("H/w Model:    %s",$model);
+printf("Server type:  %s",$svrtype);
+printf("O/S:          %s",$OS);
+printf("Kernel:       %s",$kernel);
+printf("Memory:\n%s\n",$memswap);
+print"\n###########################\n";
+printf("BLOCK DEVICES");
+print"\n###########################\n";
+printf("%s",$blkinfo);
+print"\n###########################\n";
+printf("FILESYSTEMS");
+print"\n###########################\n";
+printf("%s",$filesystems);
+print"\n###########################\n";
+
+}
+
