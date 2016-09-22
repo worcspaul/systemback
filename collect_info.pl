36f6a4d899a347dc0e16caeeacbbaa2eca4d5bac
Paul Walsh <paul.walsh@first-utility.com>
2016-09-22 15:57:48 +0100

    added LVM

--- collect_info.pl
+++ collect_info.pl
@@ -1,58 +1,90 @@
-
+#!/usr/bin/perl
+#
 #====================================================================
 # collect_info	 - Collect useful information
 #====================================================================
-sub collect_info {
-
-   $hostname=`hostname`;
-         $memswap= `free -m`;
-         $hostid=`hostname`;
-         $kernel=`uname -r`;
-         $model= "echo \"N/A\"";
-         $type= "echo \"N/A\"";
-         $blkinfo=`lsblk`;
-         $filesystems= `df -hP`;
-
-   if (-e "/etc/redhat-release") {
-      $OS = `cat /etc/redhat-release`;
-   } else {
-      $OS = `cat /etc/issue`;
-   }
-   chop($hostname);
-   $resolver = new Net::DNS::Resolver();
-   $reply = $resolver->query($hostname,'A');
-   if ($reply) {
-     foreach my $rr ($reply->answer) {
-       next unless $rr->type eq "A";
-      $ipaddr=$rr->address;
-     }
-   }
+@IPINFO;
+$INFOFILE="/INFOFILE";
+$ipcount=0;
+$hostname=`hostname`;
+$memswap= `free -m`;
+$hostid=`hostname`;
+$kernel=`uname -r`;
+$blkinfo=`lsblk`;
+$filesystems= `df -hP`;
+$pv=`pvdisplay`;
+chop($pv);
+printf("Length: %d\n",length($pv));
+if (length($pv) > 0) {
+   $LVM=1;
 }
 
-#====================================================================
-# write_info	 - write info to file
-#====================================================================
-#
-sub write_info {
-print"#############################\n";
-print"######## SYSTEM INFO ########\n";
-print"#############################\n\n";
-printf("Hostname:     %s",$hostname);
-printf("IP Address:   %s",$ipaddr);
-printf("H/w Model:    %s",$model);
-printf("Server type:  %s",$svrtype);
-printf("O/S:          %s",$OS);
-printf("Kernel:       %s",$kernel);
-printf("Memory:\n%s\n",$memswap);
-print"\n###########################\n";
-printf("BLOCK DEVICES");
-print"\n###########################\n";
-printf("%s",$blkinfo);
-print"\n###########################\n";
-printf("FILESYSTEMS");
-print"\n###########################\n";
-printf("%s",$filesystems);
-print"\n###########################\n";
-
+if (-e "/etc/redhat-release") {
+   $OS = `cat /etc/redhat-release`;
+} else {
+   $OS = `cat /etc/issue`;
 }
-
+#     inet 10.0.2.15/24 brd 10.0.2.255 scope global dynamic enp0s3
+#     inet 10.180.21.44/8 brd 10.255.255.255 scope global eno1
+   
+open(OUT,">$INFOFILE");
+open(IPINFO,"ip addr |grep \"inet \"|");
+while ($rec = <IPINFO>) {
+   chop($rec);
+   if (($rec =~ /inet (\S+) brd (\S+) scope global dynamic (\S+)$/) || ($rec =~ /inet (\S+) brd (\S+) scope global (\S+)$/) ) {
+      $ipaddr=$1;
+      $broadcast = $2;
+      $interface = $3;
+      $IPREC=sprintf("%s:%s:%s",$ipaddr, $broadcast, $interface);
+      push(@IPINFO,$IPREC);
+      $ipcount++;
+   }
+}
+printf(OUT "#############################\n");
+printf(OUT "######## SYSTEM INFO ########\n");
+printf(OUT "#############################\n\n");
+printf(OUT "Hostname:     %s",$hostname);
+printf(OUT "O/S:          %s",$OS);
+printf(OUT "Kernel:       %s",$kernel);
+printf(OUT "Memory:\n%s\n",$memswap);
+if ($ipcount > 0) {
+   printf(OUT "\n###########################\n");
+   printf(OUT "NETWORK");
+   printf(OUT "\n###########################\n");
+   printf(OUT "\nInterface	IP Address	Broadcast\n");
+   foreach $irec (@IPINFO) {
+      ($inet,$brd,$int) = split(':',$irec);
+      printf(OUT "$int		$inet	$brd\n");
+   }
+}
+printf(OUT "\n###########################\n");
+printf(OUT "BLOCK DEVICES");
+printf(OUT "\n###########################\n");
+printf(OUT "%s",$blkinfo);
+printf(OUT "\n###########################\n");
+printf(OUT "FILESYSTEMS");
+printf(OUT "\n###########################\n");
+printf(OUT "%s",$filesystems);
+if ($LVM == 1) {
+   printf(OUT "\n###########################\n");
+   printf(OUT "LOGICAL VOLUME MANAGEMENT");
+   printf(OUT "\n###########################\n");
+   printf(OUT "\nPHYSICAL VOLUMES\n");
+   open(PVS,"pvs|");
+   while ($pvrec = <PVS>) {
+      printf(OUT "%s",$pvrec);
+   }
+   close(PVS);
+   printf(OUT "\nVOLUME GROUPS\n");
+   open(VGS,"vgs|");
+   while ($vgrec = <VGS>) {
+      printf(OUT "%s",$vgrec);
+   }
+   close(VGS);
+   printf(OUT "\nLOGICAL VOLUMES\n");
+   open(LVS,"lvs|");
+   while ($lvrec = <LVS>) {
+      printf(OUT "%s",$lvrec);
+   }
+   close(LVS);
+}
