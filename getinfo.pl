6b0e96a48d33c7a11b2f4cef810a2ce91113824c
Paul Walsh <paul.walsh@first-utility.com>
2016-09-22 13:51:24 +0100

    to be deleted

--- getinfo.pl
+++ getinfo.pl
@@ -1,85 +1,62 @@
 #!/usr/bin/perl
 
-use Socket;
-use Net::DNS;
+#====================================================================
+# collect_info	 - Collect useful information
+#====================================================================
+sub collect_info {
 
-# getinfo.pl	-	Collect important information prior to performing a backup
+   $hostname=`hostname`;
+         $memswap= `free -m`;
+         $hostid=`hostname`;
+         $kernel=`uname -r`;
+         $model= "N/A";
+         $type= "N/A";
+         $blkinfo=`lsblk`;
+         $filesystems= `df -hP`;
 
-# This script is run prior to a system backup (using dump) being taken.  Its puprose is to gather
-# information which should prove useful in the event of system recovery being required.
-
-# Whilst it would be useful for the host to have the package  facter  installed, this cannot be 
-# guaranteed, so populate the hash with alternative commands 
-
-# $facter_path=`which wibble`;
-$facter_path=`which facter`;
-chop($facter_path);    #get rid of trailling CR/NL
-
-if (length($facter_path) ne 0) {
-   %info_cmds = (
-      "memswap","free -m",
-      "hostid","facter fqdn",
-      "kernel","uname -r",
-      "model", "facter productname",
-      "type", "facter virtual",
-      "blkinfo","lsblk",
-      "filesystems", "df -hP"
-   );
-
-} else {
-   %info_cmds = (
-      "memswap", "free -m",
-      "hostid","hostname",
-      "kernel","uname -r",
-      "model", "echo \"N/A\"",
-      "type", "echo \"N/A\"",
-      "blkinfo","lsblk",
-      "filesystems", "df -hP"
-   );
+   if (-e "/etc/redhat-release") {
+      $OS = `cat /etc/redhat-release`;
+   } else {
+      $OS = `cat /etc/issue`;
+   }
+#   chop($hostname);
+#   $resolver = new Net::DNS::Resolver();
+#   $reply = $resolver->query($hostname,'A');
+#   if ($reply) {
+#     foreach my $rr ($reply->answer) {
+#       next unless $rr->type eq "A";
+#      $ipaddr=$rr->address;
+#     }
+#   }
 }
 
-if (-e "/etc/redhat-release") {
-   $OS = `cat /etc/redhat-release`;
-} else {
-   $OS = `cat /etc/issue`;
-}
-$hostname=`$info_cmds{"hostid"}`;
-chop($hostname);
-$resolver = new Net::DNS::Resolver();
-$reply = $resolver->query($hostname,'A');
-if ($reply) {
-  foreach my $rr ($reply->answer) {
-    next unless $rr->type eq "A";
-$ipaddr=$rr->address;
-  }
-}
-
+#====================================================================
+# write_info	 - write info to file
+#====================================================================
+#
+sub write_info {
 print"#############################\n";
 print"######## SYSTEM INFO ########\n";
 print"#############################\n\n";
-printf("Hostname:     %s",`$info_cmds{"hostid"}`);
-printf("IP Address:   %s\n",$ipaddr);
-printf("H/w Model:    %s",`$info_cmds{"model"}`);
-printf("Server type:  %s\n",`$info_cmds{"type"}`);
+printf("Hostname:     %s",$hostname);
+#printf("IP Address:   %s",$ipaddr);
+printf("H/w Model:    %s",$model);
+printf("Server type:  %s",$svrtype);
 printf("O/S:          %s",$OS);
-printf("Kernel:       %s\n",`$info_cmds{"kernel"}`);
-$mem=`$info_cmds{"memswap"}`;
-printf("Memory:\n%s\n",$mem);
-$blkinfo=`$info_cmds{"blkinfo"}`;
+printf("Kernel:       %s",$kernel);
+printf("Memory:\n%s\n",$memswap);
 print"\n###########################\n";
 printf("BLOCK DEVICES");
 print"\n###########################\n";
 printf("%s",$blkinfo);
-$fsinfo=`$info_cmds{"filesystems"}`;
 print"\n###########################\n";
 printf("FILESYSTEMS");
 print"\n###########################\n";
-printf("%s",$fsinfo);
+printf("%s",$filesystems);
 print"\n###########################\n";
 
-## foreach $cmd (keys %info_cmds) {
-## $command = $info_cmds{$cmd};
-## #print "$command\n";
-## print `$command`;
-## #system($command);
-## }
+}
+
+&collect_info;
+&write_info;
+
